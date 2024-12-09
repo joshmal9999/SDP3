746721b4d686b9f93fafd3ef408de23c3554eae4
Jaewook <jaewook02094@hanyang.ac.kr>
2024-12-10 01:21:07 +0900

    test4

--- src/screen/EditorScreen.java
+++ src/screen/EditorScreen.java
@@ -124,23 +124,4 @@ public class EditorScreen extends Screen {
         }
         return false;
     }
-
-    /**
-     * Determines if a given string is a palindrome.
-     *
-     * @param text The string to check.
-     * @return True if the string is a palindrome, false otherwise.
-     */
-    public boolean isPalindrome(String text) {
-        int left = 0;
-        int right = text.length() - 1;
-        while (left < right) {
-            if (text.charAt(left) != text.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
 }
